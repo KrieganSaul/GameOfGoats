20ea629184bd3c215de1329c87d209fe87a7bbec
torrente <torrente@users.noreply.github.com>
2016-07-21 07:44:03 -0500

    Added Game Assets

---
+++ Assets/Code/Player/MouseCharacterControl.cs
@@ -0,0 +1,45 @@
+/*======================================================================
+ * MouseCharacterControl
+ * 
+ * Description: This class rotates the player to reposition the forward
+ * movement. It does not rotate the head up and down nor does it
+ * actually change the position coordinates. This code goes on the
+ * actual player object.
+ * ====================================================================*/
+
+using UnityEngine;
+using System.Collections;
+
+public class MouseCharacterControl : MonoBehaviour {
+	
+	private float rotationX;
+
+	private float sensitivityX;
+	
+	private Vector3 moveDirection;
+	
+	void Start ()
+	{
+		
+		sensitivityX = 15F;
+		
+		moveDirection = Vector3.zero;
+			
+		rotationX = 0F;
+
+	}// END START
+		
+		
+	void Update ()
+	{
+			
+			rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+		
+			moveDirection = new Vector3(0,rotationX,0);
+			
+			transform.localEulerAngles = moveDirection;
+		
+	}//END UPDATE
+	
+	
+}//END MOUSECHARACTERCONTROL
---
+++ Assets/Code/Player/MouseLook.cs
@@ -0,0 +1,46 @@
+/*======================================================================
+ * MouseLook
+ * 
+ * Description: This class rotates the camera in order to allow the
+ * player to look up and down. To use it properly, place this code on
+ * a camera which is the child of the player. 
+ * ====================================================================*/
+
+using UnityEngine;
+using System.Collections;
+
+public class MouseLook : MonoBehaviour {
+	
+	private float minimumY;
+	private float maximumY;
+	
+	private float rotationY;
+	private float sensitivityY;
+	
+	private Vector3 moveDirection;
+	
+	
+	void Start ()
+	{
+		
+		Cursor.visible = false;
+		
+		minimumY = -60F;
+		maximumY = 60F;
+		
+		rotationY = 0F;
+		sensitivityY = 15F;
+	}
+	
+	void Update ()
+	{
+		
+			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+			rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+		
+			moveDirection = new Vector3(-rotationY,0,0);
+			transform.localEulerAngles = moveDirection;
+	
+	}
+	
+}//END MouseLook
